b12ec1168974c570eb964beda07a58aadd46ed4d
VikaVelhan <106451622+VikaVelhan@users.noreply.github.com>
2022-11-21 20:48:19 +0000

    Deploying to gh-pages from @ VikaVelhan/goit-js-hw-09@13bbc5ef04b8e075ec4691360f34b7e2f7f793de 🚀

--- 01-color-switcher.dc362d9d.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(function(){timerId=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.startBtn.disabled=!0})),t.stopBtn.addEventListener("click",(function(){clearInterval(timerId),t.startBtn.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.dc362d9d.js.map
