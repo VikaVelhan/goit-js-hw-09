bfb56fe5a75d0b33db2c50d8ef65bc4989c96494
VikaVelhan <velganvika@gmail.com>
2022-11-22 00:20:45 +0200

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,37 @@
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix';
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promis = new Promise((resolt, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolt({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promis;
+}
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onSabmite);
+
+function onSabmite(event) {
+  event.preventDefault();
+  let delay = Number(event.currentTarget.delay.value);
+  const amount = Number(event.currentTarget.amount.value);
+  const step = Number(event.currentTarget.step.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
 }
