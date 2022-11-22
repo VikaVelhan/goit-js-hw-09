4bba89de0b8115aa28918bd813933bec17b3390d
VikaVelhan <velganvika@gmail.com>
2022-11-23 01:14:16 +0200

    update task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,68 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix';
+
+const refs = {
+  daysEl: document.querySelector('[data-days]'),
+  hoursEl: document.querySelector('[data-hours]'),
+  minutesEl: document.querySelector('[data-minutes]'),
+  secondsEl: document.querySelector('[data-seconds]'),
+  btnStart: document.querySelector('[data-start]'),
+  inputEl: document.querySelector('#datetime-picker'),
+};
+
+refs.btnStart.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= new Date()) {
+      return window.alert('Please choose a date in the future');
+    } else {
+      refs.btnStart.disabled = false;
+    }
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(refs.inputEl, options);
+
+refs.btnStart.addEventListener('click', onTimer);
+function onTimer() {
+  setInterval(() => {
+    refs.btnStart.disabled = true;
+    const timeNow = Date.now();
+    const startTime = new Date(refs.inputEl.value);
+    const deltaTime = startTime - timeNow;
+    //console.log(timeNow);
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+    console.log(`${days}:${hours}:${minutes}:${seconds}`);
+  }, 1000);
+}
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
