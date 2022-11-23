8670a2b6bc0fadbcbed9a9e56441bcb66380db9e
VikaVelhan <velganvika@gmail.com>
2022-11-23 23:22:46 +0200

    task3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -20,28 +20,37 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (selectedDates[0] <= new Date()) {
-      return window.alert('Please choose a date in the future');
+      Notify.failure('Please choose a date in the future');
     } else {
       refs.btnStart.disabled = false;
     }
-    console.log(selectedDates[0]);
+    //console.log(selectedDates[0]);
   },
 };
 
 flatpickr(refs.inputEl, options);
 
 refs.btnStart.addEventListener('click', onTimer);
+
 function onTimer() {
-  setInterval(() => {
-    refs.btnStart.disabled = true;
+  refs.btnStart.disabled = true;
+  const intervalId = setInterval(() => {
     const timeNow = Date.now();
     const startTime = new Date(refs.inputEl.value);
     const deltaTime = startTime - timeNow;
-    //console.log(timeNow);
+
     const { days, hours, minutes, seconds } = convertMs(deltaTime);
-    console.log(`${days}:${hours}:${minutes}:${seconds}`);
+    refs.daysEl.textContent = `${days}`;
+    refs.hoursEl.textContent = `${hours}`;
+    refs.minutesEl.textContent = `${minutes}`;
+    refs.secondsEl.textContent = `${seconds}`;
+    //console.log(`${days}:${hours}:${minutes}:${seconds}`);
+    if (deltaTime < 1000) {
+      clearInterval(intervalId);
+    }
   }, 1000);
 }
+
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
